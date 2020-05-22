6c7ddf56d7e1d383e8474d657bcd7110f0e0482c
hengguan <2016223040045@stu.scu.edu.cn>
2020-05-22 22:15:52 +0800

    opt conv2d

--- nn/conv.py
+++ nn/conv.py
@@ -37,25 +37,34 @@ class Conv2d:
 
         # print('padding')
         if self.padding:
-            out = np.zeros((n, h, w, self.out_channels))
+            out_h, out_w = h, w
+            # out = np.zeros((n, h, w, self.out_channels))
             padding = (h - 1)*self.stride - h + self.k_size 
             padding = (int(padding//2), int(padding//2)) if padding % 2 ==0 \
                 else (int(padding/2+0.5), int(padding/2-0.5))
             x = np.pad(x, ((0, 0), padding, padding, (0, 0)), mode='constant', constant_values=0)
             print(x.shape)
         else:
+            out_h = int((h-self.k_size)/self.stride + 1)
+            out_w = int((h-self.k_size)/self.stride + 1)
             # out_size = (x_shape[1]-self.k_size)/self.stride + 1
-            out = np.zeros((n, int((h-self.k_size)/self.stride + 1), \
-                int((h-self.k_size)/self.stride + 1), self.out_channels))
+            # out = np.zeros((n, int((h-self.k_size)/self.stride + 1), \
+            #     int((h-self.k_size)/self.stride + 1), self.out_channels))
 
-        new_h = x.shape[1]
+        # new_h = x.shape[1]
         print('convolution')
-        for i in range(0, new_h-self.k_size+1, self.stride):
-            for j in range(0, new_h-self.k_size+1, self.stride):
-                row = x[:, j:j+self.k_size, i:i+self.k_size, :].reshape(n, -1)
-                row_res = row.dot(self.weights)
-                out[:, j, i, :] = row_res.reshape(n, 1, 1, self.out_channels)
-                
+        out = np.zeros((n, out_h, out_w, self.out_channels))
+        # blocks = []# x[:, :self.k_size, :self.k_size, :].reshape(n, 1, -1)
+        for i in range(out_h):
+            for j in range(out_w):
+                sh, sw = i*self.stride, j*self.stride
+                block = x[:, sh:sh+self.k_size, sw:sw+self.k_size, :].reshape(n, -1)
+                out[:, i, j, :] = block.dot(self.weights).reshape(n, 1, 1, self.out_channels)
+        #         blocks.append(block)
+        # feature = np.concatenate(blocks, axis=1)
+        # out = feature.dot(self.weights).reshape(n, out_h, out_w, self.out_channels)
+        # out[:, j, i, :] = row_res.reshape(n, 1, 1, self.out_channels)
+
         self.x_grad = self.weights
         self.w_grad = self.feature
 
