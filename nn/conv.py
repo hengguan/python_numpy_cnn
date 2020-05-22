070948aba87c033ec2c647e3ca9c91cd480d7d23
hengguan <2016223040045@stu.scu.edu.cn>
2020-05-22 20:57:43 +0800

    conv2d opt

--- nn/conv.py
+++ nn/conv.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 class Conv2d:
+
     def __init__(
         self,
         in_channels,
@@ -21,35 +22,39 @@ class Conv2d:
             (self.k_size, self.k_size, self.in_channels, self.out_channels))
         self.bias = 0.1 * np.random.standard_normal(self.out_channels)
         self.weights = self.weights.reshape(-1, self.out_channels)
-        print(self.weights)
+        
         self.feature = None
 
     def __call__(self, x):
         if not isinstance(x, np.ndarray):
-            raise 'input is not numpy.ndarray'
+            raise ValueError('dtype of input is not numpy.ndarray')
+        assert len(x.shape)==4, "the shape of input must be format as [n, h, w, c]"
         self.feature = x
-        x_shape = x.shape
-        assert len(x_shape) == 4
+        n, h, w, c = x.shape
+        assert c==self.in_channels, \
+            "the channel({}) of input feature is equal to the channel({}) of weights." \
+                .format(c, self.in_channels)
 
         # print('padding')
         if self.padding:
-            out = np.zeros((x_shape[0], x_shape[1], x_shape[2], self.out_channels))
-            padding = (x_shape[1] - 1)*self.stride - x_shape[1] + self.k_size 
+            out = np.zeros((n, h, w, self.out_channels))
+            padding = (h - 1)*self.stride - h + self.k_size 
             padding = (int(padding//2), int(padding//2)) if padding % 2 ==0 \
                 else (int(padding/2+0.5), int(padding/2-0.5))
             x = np.pad(x, ((0, 0), padding, padding, (0, 0)), mode='constant', constant_values=0)
             print(x.shape)
         else:
             # out_size = (x_shape[1]-self.k_size)/self.stride + 1
-            out = np.zeros((x_shape[0], int((x_shape[1]-self.k_size)/self.stride + 1), \
-                int((x_shape[1]-self.k_size)/self.stride + 1), self.out_channels))
+            out = np.zeros((n, int((h-self.k_size)/self.stride + 1), \
+                int((h-self.k_size)/self.stride + 1), self.out_channels))
 
+        new_h = x.shape[1]
         print('convolution')
-        for i in range(0, x.shape[1]-self.k_size+1, self.stride):
-            for j in range(0, x.shape[1]-self.k_size+1, self.stride):
-                row = x[:, j:j+self.k_size, i:i+self.k_size, :].reshape(x_shape[0], -1)
+        for i in range(0, new_h-self.k_size+1, self.stride):
+            for j in range(0, new_h-self.k_size+1, self.stride):
+                row = x[:, j:j+self.k_size, i:i+self.k_size, :].reshape(n, -1)
                 row_res = row.dot(self.weights)
-                out[:, j, i, :] = row_res.reshape(x_shape[0], 1, 1, self.out_channels)
+                out[:, j, i, :] = row_res.reshape(n, 1, 1, self.out_channels)
                 
         self.x_grad = self.weights
         self.w_grad = self.feature
