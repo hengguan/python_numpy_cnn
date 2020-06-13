087dec722cce0af8e279943ec838d327246cc8f8
hengguan <2016223040045@stu.scu.edu.cn>
2020-06-13 22:28:43 +0800

    opt conv

--- nn/conv.py
+++ nn/conv.py
@@ -19,7 +19,7 @@ class Conv2d:
         assert in_channels is not None and out_channels is not None
 
         self.weights = 0.1*np.random.standard_normal(
-            (self.k_size, self.k_size, self.in_channels, self.out_channels))
+            (self.in_channels, self.k_size, self.k_size, self.out_channels))
         self.bias = 0.1 * np.random.standard_normal(self.out_channels)
         self.weights = self.weights.reshape(-1, self.out_channels)
         
@@ -34,30 +34,36 @@ class Conv2d:
         assert c==self.in_channels, \
             "the channel({}) of input feature is equal to the channel({}) of weights." \
                 .format(c, self.in_channels)
-
+        print(self.feature.shape, x.shape)
         # print('padding')
         if self.padding:
-            out_h, out_w = h, w
+            temp_h, temp_w = h/self.stride, w/self.stride
+            out_h = int(temp_h)+1 if (temp_h)%1!=0 else int(temp_h)
+            out_w = int(temp_w)+1 if (temp_w)%1!=0 else int(temp_w)
             # out = np.zeros((n, h, w, self.out_channels))
-            padding = (h - 1)*self.stride - h + self.k_size 
-            padding = (int(padding//2), int(padding//2)) if padding % 2 ==0 \
-                else (int(padding/2+0.5), int(padding/2-0.5))
-            x = np.pad(x, ((0, 0), padding, padding, (0, 0)), mode='constant', constant_values=0)
+            pad_h = (out_h - 1)*self.stride - h + self.k_size 
+            pad_w = (out_w - 1)*self.stride - w + self.k_size
+            self.padding_h = int(pad_h//2) if pad_h % 2 ==0 else int(pad_h/2)+1
+            self.padding_w = int(pad_w//2) if pad_w % 2 ==0 else pad_w-self.padding_h
+            pad = (self.padding_h, self.padding_w)
+            x = np.pad(x, ((0, 0), pad, pad, (0, 0)), mode='constant', constant_values=0)
             # print(x.shape)
         else:
             out_h = int((h-self.k_size)/self.stride + 1)
-            out_w = int((h-self.k_size)/self.stride + 1)
+            out_w = int((w-self.k_size)/self.stride + 1)
+            self.padding_h, self.padding_w = 0, 0
             # out_size = (x_shape[1]-self.k_size)/self.stride + 1
             # out = np.zeros((n, int((h-self.k_size)/self.stride + 1), \
             #     int((h-self.k_size)/self.stride + 1), self.out_channels))
-
+        print(self.feature.shape, x.shape)
         # new_h = x.shape[1]
         # print('convolution')
         out = np.zeros((n, out_h, out_w, self.out_channels))
         # blocks = []# x[:, :self.k_size, :self.k_size, :].reshape(n, 1, -1)
         for i in range(out_h):
+            sh = i*self.stride
             for j in range(out_w):
-                sh, sw = i*self.stride, j*self.stride
+                sh = j*self.stride
                 block = x[:, sh:sh+self.k_size, sw:sw+self.k_size, :].reshape(n, -1)
                 out[:, i, j, :] = block.dot(self.weights).reshape(n, 1, 1, self.out_channels)
         #         blocks.append(block)
@@ -65,7 +71,21 @@ class Conv2d:
         # out = feature.dot(self.weights).reshape(n, out_h, out_w, self.out_channels)
         # out[:, j, i, :] = row_res.reshape(n, 1, 1, self.out_channels)
 
-        self.x_grad = self.weights
-        self.w_grad = self.feature
+        # self.x_grad = self.weights
+        self.w_grad = x
 
         return out
+
+        def backword(self, dy):
+            yn, yh, yw, yc = dy.shape
+            out_c = self.w_grad.shape[-1]
+            x = np.zeros_like(self.w_grad)
+            self.w_grad = np.zeros_like(self.weights)
+            for i in yh:
+                xh = i*self.stride
+                for j in yw:
+                    xw = j*self.stride
+                    grad_y = dy[:, i, j, :].reshape(yn, 1, yc)
+                    grad_x = (grad_y * self.weights).reshape(yn, self.k_size, self.k_size, out_c)
+                    x[:, xh:xh+self.k_size, xw:xw+self.k_size, :] += grad_x
+
--- test.py
+++ test.py
@@ -1,23 +1,25 @@
 import numpy as np
 
+if __name__ == "__main__":
+    a = np.arange(6).reshape(2, 1, 3)
+    b = np.arange(108).reshape(36, 3)
+    res = a*b
+    print(res.shape)
+    m = np.mean(a, axis=-1)
+    print(m.shape)
+    b = np.arange(9).reshape(3, 3)
+    print(b*b)
+    print(b**2)
 
-a = np.array(range(150)).reshape(2, 5, 5, 3)
-# print(a)
-m = np.mean(a, axis=-1)
-print(m.shape)
-b = np.arange(9).reshape(3, 3)
-print(b*b)
-print(b**2)
+    # target = np.array([
+    #     [0, 1, 0],
+    #     [1, 0, 0],
+    #     [0, 0, 1]])
 
-# target = np.array([
-#     [0, 1, 0],
-#     [1, 0, 0],
-#     [0, 0, 1]])
+    # exp_a = np.exp(a)
+    # sum_exp = np.sum(exp_a, axis=0, keepdims=True)
+    # print(exp_a)
+    # print(sum_exp)
+    # print(exp_a/sum_exp)
 
-# exp_a = np.exp(a)
-# sum_exp = np.sum(exp_a, axis=0, keepdims=True)
-# print(exp_a)
-# print(sum_exp)
-# print(exp_a/sum_exp)
-
-# print(-target*np.log(exp_a/sum_exp))
+    # print(-target*np.log(exp_a/sum_exp))
